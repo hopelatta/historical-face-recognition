cc8ddbef61eaaa48f3b49ba394dbf8ee7e131ef9
Hope L <39097445+hopelatta@users.noreply.github.com>
2020-03-11 23:40:19 -0300

    Add files via upload

--- database.sql
+++ database.sql
@@ -15,9 +15,12 @@ INSERT INTO `users`(`username`, `password`) values('Acadia','Jodrey');
 /* Photos: name to go with photo must be the name of the photo*/
 /* Image: stores in base 64 value */
 
-CREATE TABLE `images` (
-  `pid` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `Name` varchar(200) NOT NULL,
-  `image` longtext NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS `personphoto` (
+  `id` int(11) NOT NULL,
+  `filename` varchar(500) DEFAULT NULL,
+    
+    /* File content is the photo */
+  `filecontent` longblob,
+  `defaultphoto` int(11) NOT NULL DEFAULT '0'
+)
 
--- upload.php
+++ upload.php
@@ -1,22 +1,28 @@
 
 <?php
 
-// Create database connection
-  $db = mysqli_connect("localhost", "root", "", "image_upload");
+header('Content-Type: application/json');
 
-$msg = "";
+if ( 0 < $_FILES['file']['error'] ) {
+echo '{"uploadfile":"failure","message":"' . $_FILES['file']['error'] . '"}';
+    }
+    else 
+{
+        /* Get information from file */ 
+/* File Content is the photo, and the name is what the photo was named */
+        
+$fileContent = addslashes(file_get_contents($_FILES['file']['tmp_name'])); 
+$fileName = $_FILES['file']['name'];
 
-// If upload button is clicked ...
-  if (isset($_POST['upload'])) {
-  	// Get image name
-  	$image = $_FILES['image']['name'];
-  	
-$target_dir = "storage/".basename($image);
-    
-      //Add to database: 
-      $sql = "INSERT INTO images (name, image) VALUES ('$name', '$image')";
-  	// execute query
-  	mysqli_query($db, $sql);
-      
-  
+        /* Add info to database */
+$sql = "insert into `personphoto` (`personId`, `filecontent`, `filename`) values ({$personId}, '{$fileContent}', '{$fileName},now())";
+   
+/* Checks to see if file upload is successful */
+        
+if ($db->query($sql) === TRUE) {
+echo '{"uploadfile":"successful", "id":"' . $db->insert_id . '"}'; 
+} else {
+echo '{"uploadfile":"failure", "errormessage":"' . $db->error . '"}'; 
+}
+}
 ?>
