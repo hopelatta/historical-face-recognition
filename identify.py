fe1f4159dd1dbbe84fa2172d5c65f0aba1b78c4d
Hope L <39097445+hopelatta@users.noreply.github.com>
2020-03-24 18:15:49 -0300

    Made some changes for calling compare_faces
    Still unable to import the python facial recognition library, not sure why.

--- identify.py
+++ identify.py
@@ -2,20 +2,23 @@ import face_recognition
 import json
 import sys
 
-
 #get the face encoding argument passed in
 encoding_to_find = sys.argv[1]  
-print("<p>find this face...: " + encoding_to_find + "</p>")
-
+#print("<p>find this face...: " + encoding_to_find + "</p>")
 
 #get all face encodings to compare against
 encodings_file = open("encodings.txt")
-print("<p>in this collection of faces: ")
-for encoding in encodings_file:
-    print("<br/>face encoding: " + encoding)
-print("</p>")
 
+#print("<p>in this collection of faces: ")
+#for encoding in encodings_file:
+#    print("<br/>face encoding: " + encoding)
+#print("</p>")
 
+all_face_encodings = encodings_file 
+face_image_encoding = encoding_to_find
+
+results = face_recognition.compare_faces(all_face_encodings, face_image_encoding,  tolerance=0.8)
+print("Results: " + results)
 #something like...
 #
 # to find: encoding_to_find
@@ -24,9 +27,4 @@ print("</p>")
 #results = face_recognition.compare_faces(all_face_encodings, face_image_encoding,  tolerance=0.8)
 #
 # see https://face-recognition.readthedocs.io/en/latest/face_recognition.html
-#
-#and check results
-#
-#
-#
 #
