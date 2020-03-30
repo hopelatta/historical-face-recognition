ec1a55f78ec066bb39043627105afb6205083d5d
Jamie Symonds <x97gfq@hotmail.com>
2020-03-30 15:16:29 -0300

    .

--- footer.php
+++ footer.php
@@ -1,11 +1,16 @@
-            <footer class="footer">
-                <p>All photographs on this website were created before January 1, 1949 and are therefore in the public domain (SC 1997, c24, s.58).&nbsp;&nbsp;
-                This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/2.5/ca/">Creative Commons Attribution 2.5 Canada License</a>.&nbsp;&nbsp;
-                <a rel="license" href="http://creativecommons.org/licenses/by/2.5/ca/">
-                    <img alt="Creative Commons Licence" style="border-width:0" src="https://i.creativecommons.org/l/by/2.5/ca/80x15.png" />
-                </a>
-                </p>
-            </footer>
+                    </div>
+                </div>
+            </div>
         </div>
+        <br/>
+        <br/>
+        <footer class="footer">
+            <p>All photographs on this website were created before January 1, 1949 and are therefore in the public domain (SC 1997, c24, s.58).&nbsp;&nbsp;
+            This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/2.5/ca/">Creative Commons Attribution 2.5 Canada License</a>.&nbsp;&nbsp;
+            <a rel="license" href="http://creativecommons.org/licenses/by/2.5/ca/">
+                <img alt="Creative Commons Licence" style="border-width:0" src="https://i.creativecommons.org/l/by/2.5/ca/80x15.png" />
+            </a>
+            </p>
+        </footer>
     </body>    
 </html>
--- header.php
+++ header.php
@@ -1,62 +1,59 @@
+<!DOCTYPE html>
 <html lang="en">
-    <head>
-        
-<meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-  <title>World War Database</title>
+    <title>World War Database</title>
 
-  <!-- Bootstrap core CSS -->
-  <link href="bootstrap/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-  <!-- Custom styles for this template -->
-  <link href="coverphoto.css" rel="stylesheet">
-
-    </head>
-    <body>
-    
+    <!-- Custom styles for this template -->
+    <link href="coverphoto.css" rel="stylesheet">
+  </head>
+  <body>
     <!-- Navigation -->
-  <div class="container">
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-    
-      <a class="navbar-brand" href="#">Get started</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto">
-        <li class="nav-item">
-            <a class="nav-link" href="homepage.php">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="report.php">Report</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="upload.php">Upload</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="identify.php">Identify</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="copyright.php">Copyright</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="index.php?action=logout">Logout</a>
-          </li>
-        </ul>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+            <a class="navbar-brand" href="#">Get started</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+              <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                  <a class="nav-link" href="homepage.php">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="report.php">Report</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="upload.php">Upload</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="identify.php">Identify</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="copyright.php">Copyright</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="index.php?action=logout">Logout</a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
       </div>
-      </nav>
-    </div>
-  
-
-  <!-- Page Content -->
-  <br>
-  <h1 class="mt-5"> World War Face Recognition Database </h1> 
-  
-  
-  <!-- Bootstrap core JavaScript -->
-  <script src="bootstrap/vendor/jquery/jquery.min.js"></script>
-  <script src="bootstrap/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+      <div class="row">
+        <div class="col-md-12">
+          <br/>
+          <h1 class="mt-5"> World War Face Recognition Database </h1> 
+      
+          <!-- Bootstrap core JavaScript -->
+          <script src="bootstrap/vendor/jquery/jquery.min.js"></script>
+          <script src="bootstrap/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
